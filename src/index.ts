d1458d165dcfe795f32baeb4e2f2689b87a06974
ErrorCode0 <me@errorcodezero.dev>
2024-06-05 21:48:19 -0700

    I think its done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
 import { createRestAPIClient } from "masto";
+import Snake from "./game.js";
+import type { Direction } from "./game.js";
+
+interface PollOption {
+    title: Direction,
+    votesCount: number | undefined
+}
 
 const masto = createRestAPIClient({
   url: String(process.env.URL),
   accessToken: process.env.TOKEN,
 });
+const game = new Snake;
 
-const user = await masto.v1.accounts.lookup({ acct: "@snake@botsin.space" })
-const lastStatus = await masto.v1.accounts.$select(user.id).statuses.list();
+async function postUpdate() {
+    const status = await masto.v1.statuses.create({
+        status: game.getGrid(),
+        poll: {
+            expiresIn: 3600,
+            options: game.getPossibleMoves()
+        },
+    })
 
-console.log(lastStatus[0].content);
+    return status;
+}
+
+async function getMostVotedMove() {
+    const user = await masto.v1.accounts.lookup({ acct: "@snake@botsin.space" })
+    const statuses = await masto.v1.accounts.$select(user.id).statuses.list();
+    const status = statuses[0];
+    
+    if(!status.poll || !status.poll.expired) return null;
+    let poll = []
+    try {
+        poll = <PollOption[]> status.poll.options;
+    } catch {
+        return null;
+    }
+    poll.sort(function (a, b) {
+        if (a.votesCount === undefined || b.votesCount === undefined) {
+            return 0;
+        }
+        return b.votesCount - a.votesCount;
+    });
+    return poll[0].title;
+}
+
+const newMove = await getMostVotedMove();
+if (!newMove) {
+    game.reset();
+} else {
+    game.move(newMove);
+}
+
+await postUpdate();
+
+setInterval(async () => {
+    const newMove = await getMostVotedMove();
+    if (!newMove) {
+        game.reset();
+    } else {
+        game.move(newMove);
+    }
+
+    await postUpdate();
+}, 1800000)
